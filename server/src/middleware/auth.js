ffc3e9c8d8e4f0217cf07dfdbed2d17263854a9e
Rauulll <nyaokeraul003@gmail.com>
2024-05-22 17:47:12 +0300

    Tokenization

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+
+const authMiddleware = (req, res, next) => {
+  // Get the token from the request headers
+  const token = req.headers.authorization && req.headers.authorization.split(' ')[1]
+
+  // Check if token exists
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' })
+  }
+
+  try {
+    // Verify the token
+    const decoded = jwt.verify(token, process.env.jwt_secret)
+
+    // Attach the decoded user information to the request object
+    req.user = decoded.user
+
+    // Call the next middleware or route handler
+  } catch (error) {
+    console.log('JWT verify error:', error.message)
+    return res.status(403).json({ message: 'Unauthorized' })
+  }
+  return next()
+}
+
+module.exports = authMiddleware
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,7 @@
 const userController = require('./controllers/userController')
 const userControllerPolicy = require('./policy/userControllerPolicy')
+// const manufacturerController = require('./controllers/ManufacturerController')
+const auth = require('./middleware/auth')
 
 module.exports = (app) => {
   app.post('/signup',
@@ -9,4 +11,7 @@ module.exports = (app) => {
   app.post('/signin',
     userController.signIn
   )
+  app.post('/manufacturer', auth, (req, res) => {
+    res.send('If you see this, you are authenticated')
+  })
 }
